12c1c2300665a20e6c802bf5cfb994c9294b036f
Yam Alao <afk.puma@gmail.com>
2023-11-16 03:42:52 -0300

    final commit

--- Draw_Staircases/src/App.java
+++ Draw_Staircases/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numSteps = 5;
+        int stepWidth = 3;
+
+        for (int i = 0; i < numSteps; i++) {
+            for (int j = 0; j < (i + 1) * stepWidth; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
